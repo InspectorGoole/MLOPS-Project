02ef6705784936462c9f2be9450d80ea53bb31a1
InspectorGoole <am6499057@gmail.com>
2025-09-08 07:10:32 +0400

    data ingestion completed

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from mlproject import logger
+from mlproject.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info('Welcome to our custom logging')
+STAGE_NAME = 'Data Ingestion'
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} <<<<< started")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main() # if u call the main method of the class it will start the entire pipeline
+    logger.info(f'>>>> stage {STAGE_NAME} completed <<<<\n\nx=======x')
+except Exception as e:
+    logger.exception(e)
+    raise e 
 
